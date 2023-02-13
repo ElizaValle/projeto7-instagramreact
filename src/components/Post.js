e91d513437d77b44696eadcb342a1b6fd3b8a3b7
ElizaValle <elizabetevalle7@gmail.com>
2023-02-13 16:44:24 -0300

    versão final

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,39 +1,58 @@
+import { useState } from "react";
+
 export default function Post(props) {
-    return (
-        <div class="post">
-            <div class="topo">
-                 <div class="usuario">
-                    <img src={props.post.imgUsuario} alt={props.post.usuario} />
-                    {props.post.usuario}
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
 
-            <div class="conteudo">
-                <img src={props.post.foto} alt={props.post.nomeFoto}/>
-            </div>
+  const [coracao, setCoracao] = useState("");
 
-            <div class="fundo">
-                <div class="acoes">
-                  <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                  </div>
-                  <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                  </div>
-                </div>
+  function clicarCoracao() {
+    if(coracao === "vermelho") {
+      setCoracao(coracao);
+    }
+  }
 
-                <div class="curtidas">
-                  <img src={props.post.imgCurtida} alt={props.post.usuarioCurtida} />
-                  <div class="texto">
-                    Curtido por <strong>{props.post.usuarioCurtida}</strong> e <strong>outras {props.post.numCurtida} pessoas</strong>
-                  </div>
-                </div>
-            </div>
+  function like() {
+    clicarCoracao();
+  }
+
+  function salvar() {
+
+  }
+
+  return (
+    <div class="post" data-test="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.post.imgUsuario} alt={props.post.usuario} />
+          {props.post.usuario}
         </div>
-    );
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img onClick={like} src={props.post.foto} alt={props.post.nomeFoto} data-test="post-image" />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon className={coracao} onClick={clicarCoracao} name="heart-outline" data-test="like-post" ></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <ion-icon onClick={salvar} name="bookmark-outline" data-test="save-post"></ion-icon>
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src={props.post.imgCurtida} alt={props.post.usuarioCurtida} />
+          <div class="texto" data-test="likes-number">
+            Curtido por <strong>{props.post.usuarioCurtida}</strong> e <strong>outras {props.post.numCurtida} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Usuario() {
 
     const usuario = [
@@ -5,15 +7,29 @@ export default function Usuario() {
             imagem: "./assets/img/catanacomics.svg",
             alt: "imagem de perfil",
             nome: "catanacomics" 
-        }];
+        }
+    ];
+
+    const [nomeUsuario, setNomeUsuario] = useState("catanacomics");
+    const [imgPerfil, setImgPerfil] = useState("./assets/img/catanacomics.svg");
+
+    function clicarLapis() {
+        const novoNome = prompt("Qual é o seu nome?");
+        setNomeUsuario(novoNome);
+    }
+
+    function trocaImgPerfil() {
+        const novaImgPerfil = prompt("Insira o link da sua nova imagem de perfil");
+        setImgPerfil(novaImgPerfil);
+    }
 
     return (
         <div class="usuario">
-            {usuario.map(u => <img src={u.imagem} alt={u.alt} />)}
-            <div class="texto">
+            {usuario.map(u => <img onClick={trocaImgPerfil} src={imgPerfil} alt={nomeUsuario} data-test="profile-image" />)}
+            <div class="texto" data-test="name">
               <span>
-                <strong>{usuario.map(u => u.nome)}</strong>
-                <ion-icon name="pencil"></ion-icon>
+                <strong>{usuario.map(u => nomeUsuario)}</strong>
+                <ion-icon onClick={clicarLapis} name="pencil" data-test="edit-name" ></ion-icon>
               </span>
             </div>
         </div>
